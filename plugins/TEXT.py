661fe990362250f6cf123490baa5c68106947ff2
Rizky61 <62826869+Rizky61@users.noreply.github.com>
2021-02-02 23:14:18 +0700

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -9,7 +9,7 @@ MEGA_EMAIL = "virgiawanrizky99@gmail.com"
 MEGA_PASSWORD = "Rizkyasihk12"
 
 
-START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .By using /auth \n\n For more info /help \n\n Third-Party Website \n Support Added /update \n\n For Bot Updates  \n <a href ='https://t.me/cok_ini_username'>Chat</a>\nPlease Report Bugs  @cok_ini_username"
+START = " Hi {}  \n\nI am Drive Uploader Bot .\n\n Please Authorise To use me .By using /auth \n\n For more info /help \n\n Third-Party Website  \n <a href ='https://t.me/cok_ini_username'>Chat</a>\nPlease Report Bugs  @cok_ini_username"
 
 HELP = """   <b>AUTHORISE BOT</b> 
        Use  /auth Command Generate
@@ -40,28 +40,17 @@ NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Us
 REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @cok_ini_username "
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
 ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n report At @cok_ini_username "
-AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
+AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me\n'
 UPLOADING = "Download Complete !! \n Uploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b>Size</b> : {} MB \n\n <b>Download</b> {}"
 AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At @cok_ini_username"
-OPENLOAD = True
-DROPBOX = True
+OPENLOAD = False
+DROPBOX = False
 MEGA = False
-ZIPPYSHARE = True
 
 
-UPDATE = """ <b> Update  on  27.07.2019</b>
-            * MEGA LINK Not Support Because I No Have Account 
-            * Error Handling Improved
-
-<b> Links Supported By Bot</b>
-            * Direct Links 
-            * Openload links [Max Speed 
-              500 KBps :(   ]
-            * Dropbox links 
-            *  Mega links (only files)
-            
-            + More are in way:) """
+UPDATE = """ <b> Update  on  27.07.2010</b>
+            * Nothing
